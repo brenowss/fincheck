4bb09ec5aba3ab6807797f902e4c84baca0ccc69
brenowss <brenoc.fiorese@hotmail.com.br>
2024-03-24 20:52:05 -0300

    Add FRONTEND_URL to .env.example and enable CORS in main.ts

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -11,7 +11,14 @@ import { BankAccountsModule } from './modules/bank-accounts/bank-accounts.module
 import { TransactionsModule } from './modules/transactions/transactions.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule, BankAccountsModule, TransactionsModule],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    AuthModule,
+    CategoriesModule,
+    BankAccountsModule,
+    TransactionsModule,
+  ],
   controllers: [],
   providers: [
     {
